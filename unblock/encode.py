bd06b7d0d1af0793c511f9a878d8a925f93bf19a
Jero <jeropardor3@gmail.com>
2022-04-07 14:58:14 +0200

    .

--- unblock/encode.py
+++ unblock/encode.py
@@ -9,17 +9,24 @@ import sys
 
 
 row = 0
-line = sys.stdin.readline()
+blocks = dict()
+
+
+
+# GET DATA ---------------------------------------------------------------------
+
+
+line = sys.stdin.readline().strip()
 line = line.strip()
 size = len(line)
-blocks = {}
 
-""" print(f'#const size={size-1}.') # n: 0..n
 
-while line:
+while line and row < size:
 
-    for col in range(size):
-        if line[col] == '-':
+    # print(f'line:{line}, {size}')
+
+    for col in range(len(line)):
+        if line[col] == '.':
             continue
             
         if line[col] in blocks:
@@ -32,20 +39,54 @@ while line:
                 'DirN':col,
                 'N':row
             }
-            if line[col] == line[col+1]:
+            if col+1 < size and line[col] == line[col+1]:
                 blocks[line[col]]['orientation'] = 'hor'
                 blocks[line[col]]['DirN'] = row
                 blocks[line[col]]['N'] = col
 
-
-
-    line = sys.stdin.readline()
-    row+=1
- """
-
-
-while line:
-    print('line:' + row)
-    line = sys.stdin.readline()
+    line = sys.stdin.readline().strip()
     row+=1
 
+
+goal = line.split("=")
+blockGoal = goal[0]
+locGoal = goal[1]
+
+
+
+# PRINTING ---------------------------------------------------------------------
+
+
+# Constants
+print('\n% Constants')
+print(f'#const size={size}.')
+# print(f'#const blockGoal={blockGoal}.')
+# print(f'#const locGoal={locGoal}.')
+
+
+# Blocks
+print('\n% Blocks')
+for block in blocks:
+    print(f'block({block}).')
+
+
+# Initial state
+print('\n% Initial state')
+print('#program initial.')
+for block in blocks:
+    s  = blocks[block]['size']
+    o  = blocks[block]['orientation']
+    dn = blocks[block]['DirN']
+    n  = blocks[block]['N']
+    print(f'h(bl({block},{s},{o},{dn}), {n}).')
+
+
+# Goal
+print('\n% Goal')
+print('#program final.')
+print(f'goal :- h(bl({blockGoal},_,_,_), {locGoal}).')
+print(':- not goal.')
+
+# Printing
+# print('\n% Printing')
+# print('#show move/2.')
